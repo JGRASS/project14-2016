75f4b2037650ee94600ed3293398cbba52ba93b5
valentinaljubisavljevic9514 <vanja.vale95@gmail.com>
2016-04-26 21:54:00 +0200

    Dodati su osnovni atributi kandidata u klasu Kandidat i get i set metode za atrubute.

--- project14-2016/src/prijemni/Kandidat.java
+++ project14-2016/src/prijemni/Kandidat.java
@@ -2,4 +2,52 @@ package prijemni;
 
 public class Kandidat {
 
+	private String ime;
+	private String prezime;
+	private String maticniBroj;
+	private int brojBodovaIzSkole;
+	private int brojBodovaNaPrijemnom;
+	
+	public String getIme() {
+		return ime;
+	}
+	
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+	
+	public String getPrezime() {
+		return prezime;
+	}
+	
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	
+	public String getMaticniBroj() {
+		return maticniBroj;
+	}
+	
+	public void setMaticniBroj(String maticniBroj) {
+		this.maticniBroj = maticniBroj;
+	}
+	
+	public int getBrojBodovaIzSkole() {
+		return brojBodovaIzSkole;
+	}
+	
+	public void setBrojBodovaIzSkole(int brojBodovaIzSkole) {
+		this.brojBodovaIzSkole = brojBodovaIzSkole;
+	}
+
+	public int getBrojBodovaNaPrijemnom() {
+		return brojBodovaNaPrijemnom;
+	}
+
+	public void setBrojBodovaNaPrijemnom(int brojBodovaNaPrijemnom) {
+		this.brojBodovaNaPrijemnom = brojBodovaNaPrijemnom;
+	}
+	
+	
+	
 }
