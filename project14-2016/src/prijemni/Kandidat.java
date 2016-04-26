949e0980d37233adf0cddfba833c5080662020ce
mladenlukic25714 <maxoandroid@gmail.com>
2016-04-26 21:30:14 +0200

    Inicijalni commit, napravljene klase i paketi

---
+++ project14-2016/src/prijemni/Kandidat.java
@@ -0,0 +1,5 @@
+package prijemni;
+
+public class Kandidat {
+
+}
---
+++ project14-2016/src/prijemni/gui/PrijemniGUI.java
@@ -0,0 +1,42 @@
+package prijemni.gui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class PrijemniGUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PrijemniGUI frame = new PrijemniGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PrijemniGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
